837f79f598acb3453c716f17d6a7cbb8308239e6
Kiki <kiki2002kimo@gmail.com>
2017-11-03 09:00:41 +0800

    Add test :: WhenPlayerWonOrderIsAA_Return30vs0 (PASS)

--- Tennis.cs
+++ Tennis.cs
@@ -15,9 +15,36 @@
         //}
         public static string Scores(string winOrder)
         {
-            if(string.IsNullOrEmpty(winOrder))
-                return "0:0";
-            return "15:0";
+            int PointOfPlayerA = 0;
+            int PointOfPlayerB = 0;
+            var scores = string.Empty;
+            foreach (var winPlayer in winOrder)
+            {
+                if (winPlayer == 'A')
+                    PointOfPlayerA ++;
+                else if (winPlayer == 'B')
+                    PointOfPlayerB ++;
+            }
+
+            scores = string.Format("{0}:{1}", TranslateScore(PointOfPlayerA), TranslateScore(PointOfPlayerB));
+            return scores;
+        }
+
+        private static string TranslateScore(int points)
+        {
+            switch (points)
+            {
+                case 0:
+                    return "0";
+                case 1:
+                    return "15";
+                case 2:
+                    return "30";
+                case 3:
+                    return "40";
+                default:
+                    return "0";
+            }
         }
     }
 }
