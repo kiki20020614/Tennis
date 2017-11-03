e73e38f30b4f5d638754e6121790b96f8f8dfac9
Kiki <kiki2002kimo@gmail.com>
2017-11-03 08:38:35 +0800

    initial commit

---
+++ Tennis.cs
@@ -0,0 +1,22 @@
+﻿namespace Tennis
+{
+    public class Tennis
+    {
+        //public enum Player
+        //{
+        //    PlayerA,
+        //    PlayerB
+        //}
+
+        //public static bool IsLeapYear(int i)
+        //{
+
+        //    throw new System.NotImplementedException();
+        //}
+        public static string Scores(string winOrder)
+        {
+            if(string.Empty(winOrder))
+                return "0:0";
+        }
+    }
+}
---
+++ TennisTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tennis
+{
+    [TestFixture]
+    public class TennisTest
+    {
+        [Test]
+        public void WhenStart_Return0vs0()
+        {
+            var expected = "0:0";
+
+            Assert.AreEqual(expected, Tennis.Scores(""));
+        }
+
+        [Test]
+        public void WhenPlayerAWon_Return15vs0()
+        {
+            var expected = "15:0";
+
+            Assert.AreEqual(expected, Tennis.Scores("A"));
+        }
+    }
+}
